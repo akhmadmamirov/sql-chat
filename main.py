531fb03fc50575d38f7c93492a81ca8bd30952d6
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-11-06 22:22:56 -0500

    adding table description

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
-# Project Description:
-# About
-# SQL command generation pipeline using Langchain 
-# and ChatGPT, and subsequently employing the generated 
-# SQL to produce an output.
-
-
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import(
+from langchain.prompts import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
     MessagesPlaceholder
 )
-
+from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
-from tools.sql import run_query_tool
+
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
+
 
 load_dotenv()
 
 chat = ChatOpenAI()
+
+tables = list_tables()
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(content=f"You are an AI that has access to a SQLite database.\n{tables}"),
         HumanMessagePromptTemplate.from_template("{input}"),
-        MessagesPlaceholder(variable_name="agent_scratchpad"),
+        MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
-tools=[run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 
 agent = OpenAIFunctionsAgent(
     llm=chat,
     prompt=prompt,
     tools=tools
-    
 )
 
 agent_executor = AgentExecutor(
@@ -41,4 +38,5 @@ agent_executor = AgentExecutor(
     tools=tools
 )
 
-agent_executor("How many users are in the database?")
+agent_executor("How many users have provided a shipping address?")
+# agent_executor("how many users are there?")
