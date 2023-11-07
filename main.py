14e79a63031c086e762f3f4fe33ddffa0cb6c211
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-11-07 13:22:52 -0500

    adding memory

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from langchain.prompts import (
 )
 from langchain.schema import SystemMessage
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
-from dotenv import load_dotenv
+from langchain.memory import ConversationBufferMemory
 
+from dotenv import load_dotenv
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
 
@@ -19,11 +20,13 @@ tables = list_tables()
 prompt = ChatPromptTemplate(
     messages=[
         SystemMessage(content=f"You are an AI that has access to a SQLite database.\n{tables}"),
+         MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad")
     ]
 )
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 agent = OpenAIFunctionsAgent(
@@ -35,7 +38,8 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory
 )
 
 agent_executor("Summarize top 5 most popular products. Write the results to a report file")
